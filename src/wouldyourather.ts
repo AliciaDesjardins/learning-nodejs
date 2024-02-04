c52ac0a5dbc476982ffb02d5b9c168ea46a4c736
AliciaDesjardins <123902431+AliciaDesjardins@users.noreply.github.com>
2024-02-04 19:41:53 +0000

    list

--- src/wouldyourather.ts
+++ src/wouldyourather.ts
@@ -1,14 +1,30 @@
 const prompt = require("prompt-sync")({sigint:true});
 
+const questions: string[] = [
+    "would you rather have 1 million dollars(type 1) or have never ending money(type 2)?",
+    "would you rather have all the answers in the world (type 1) or get all A+'s on your report card (type 2)?",
+];
+
+const answerType1: string[] = [
+    "you would have a limit!",
+    "you would know every thing you need to know"
+];
+
+const answerType2: string[] = [
+    "Dont forget to share",
+    "you would never need to study"
+];
+
 let goodAnswer: number =0; // Can change
 
-const answer1 = prompt("would you rather have 1 million dollars(type 1) or have never ending money(type 2)?")??"";
-const answer1LowerCase = answer1.toLowerCase(); // Cannot change
+// QUESTION 1 =====> in the list 0
+const answer1:string = prompt(questions[0])??"";
+const answer1LowerCase:string = answer1.toLowerCase(); // Cannot change
 
 if (answer1LowerCase =="1"){
-    console.log("you would have a limit!");
+    console.log(answerType1[0]);
 } else if (answer1LowerCase=="2"){
-    console.log("Dont forget to share");
+    console.log(answerType2[0]);
     goodAnswer=goodAnswer+1;
 } else {
     console.log("You did not answer the question properly");
@@ -16,15 +32,17 @@ if (answer1LowerCase =="1"){
 
 console.log("Your score is " + goodAnswer + " out of 2");
 
-const answer2 = prompt("would you rather have all the answers in the world (type 1) or get all A+'s on your report card (type 2)?")??"";;
+// QUESTION 2 =====> in the list 1
+const answer2 = prompt(questions[1])??"";;
 const answer2LowerCase = answer2.toLowerCase(); // Cannot change
 if (answer2LowerCase =="1"){
-    console.log("you would know every thing you need to know");
+    console.log(answerType1[1]);
+    goodAnswer=goodAnswer+1;
 }
 else if (answer2LowerCase=="2"){
-    console.log("you would never need to study");
+    console.log(answerType2[1]);
 }else {
-    console.log("You need to answer by 1 or 2.");
+    console.log("You need to answer by 1 or 2. Unfortunately, you answered:" + answer2);
 }
-goodAnswer=goodAnswer+1;
+
 console.log("Your score is " + goodAnswer + " out of 2");
