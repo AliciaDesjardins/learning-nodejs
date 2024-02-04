e762a6e4e062635ff68a42fdf5957d4936397965
AliciaDesjardins <123902431+AliciaDesjardins@users.noreply.github.com>
2024-02-04 20:00:20 +0000

    loop and list

--- src/wouldyourather.ts
+++ src/wouldyourather.ts
@@ -3,46 +3,35 @@ const prompt = require("prompt-sync")({sigint:true});
 const questions: string[] = [
     "would you rather have 1 million dollars(type 1) or have never ending money(type 2)?",
     "would you rather have all the answers in the world (type 1) or get all A+'s on your report card (type 2)?",
+    "would you rather be rich and ugly (type 1) or poor and beautiful(type 2)?"
 ];
 
 const answerType1: string[] = [
     "you would have a limit!",
-    "you would know every thing you need to know"
+    "you would know every thing you need to know",
+    "You might be alone with your money"
 ];
 
 const answerType2: string[] = [
     "Dont forget to share",
-    "you would never need to study"
+    "you would never need to study",
+    "You might have a lot of friend"
 ];
 
+const rightAnswer: number[] = [1,0,1];
+
 let goodAnswer: number =0; // Can change
-
-// QUESTION 1 =====> in the list 0
-const answer1:string = prompt(questions[0])??"";
-const answer1LowerCase:string = answer1.toLowerCase(); // Cannot change
-
-if (answer1LowerCase =="1"){
-    console.log(answerType1[0]);
-} else if (answer1LowerCase=="2"){
-    console.log(answerType2[0]);
-    goodAnswer=goodAnswer+1;
-} else {
-    console.log("You did not answer the question properly");
+for (let i=0;i<questions.length;i++){
+    const answer:string = prompt(questions[i])??"";
+    if (answer == "1"){
+        console.log(answerType1[i]);
+    } else if (answer == "2"){
+        console.log(answerType2[i]);
+    } else {
+        console.log("You did not answer the question properly");
+    }
+    if(Number(answer)-1==rightAnswer[i]){
+        goodAnswer++;
+    }
+    console.log("Your score is " + goodAnswer + " out of "+questions.length);
 }
-
-console.log("Your score is " + goodAnswer + " out of 2");
-
-// QUESTION 2 =====> in the list 1
-const answer2 = prompt(questions[1])??"";;
-const answer2LowerCase = answer2.toLowerCase(); // Cannot change
-if (answer2LowerCase =="1"){
-    console.log(answerType1[1]);
-    goodAnswer=goodAnswer+1;
-}
-else if (answer2LowerCase=="2"){
-    console.log(answerType2[1]);
-}else {
-    console.log("You need to answer by 1 or 2. Unfortunately, you answered:" + answer2);
-}
-
-console.log("Your score is " + goodAnswer + " out of 2");
