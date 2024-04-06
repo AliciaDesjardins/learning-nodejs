a02650676984847302b5622d1a65bc6a03d945f8
AliciaDesjardins <123902431+AliciaDesjardins@users.noreply.github.com>
2024-04-06 18:23:46 +0000

    Code clean up with more functions

--- src/game1.ts
+++ src/game1.ts
@@ -1,179 +1,70 @@
 import prompt_sync from "prompt-sync";
 const promptsync = prompt_sync({ sigint: true });
+enum Actions {
+    Unknown = 0,
+    Heal = 1,
+    Attack = 2
+}
 
-// Ask names
-const player1Name: string = promptsync("Player 1 name: ") ?? "";
-const player2Name: string = promptsync("Player 2 name: ") ?? "";
+enum AttackTypes {
+    Unknown = 0,
+    Addition = 1,
+    Substraction = 2,
+    Multiplication = 3,
+    Division = 4
+}
 
- // Recharge Memory
-const player1Actions: boolean[]  = [false,false,false,false];
-const player2Actions: boolean[]  = [false,false,false,false];
+interface Player {
+    name: string;
+    actions: boolean[];
+    health: number;
+}
 
-// Start game
-let p1Health: number = minMax(48, 52);
-let p2Health: number = minMax(48, 52);
-let isP1Turn: boolean = true;
-let roundNumber = 0;
-while (p1Health > 0 && p2Health > 0) {
-    roundNumber = roundNumber + 1;
-    console.log("========================================");
-    console.log("================ Round " + roundNumber + " ==============");
-    console.log("========================================");
-    if (isP1Turn) {
-        console.log(
-            player1Name + " " + p1Health + " vs " + p2Health + " " + player2Name,
-        );
-    } else {
-        console.log(
-            player2Name + " " + p2Health + " vs " + p1Health + " " + player1Name,
-        );
-    }
-    console.log("What do you want to do?");
-    console.log("1. Heal");
-    console.log("2. Attack");
-    const actionNumber: number = Number(promptsync("Choose an action: ") ?? "");
-    if (actionNumber == 1) {
-        const actionType: number = minMax(1, 4); // Random between 1 and 4
-        let point = 0;
-        if(actionType==1){
-            const result = askAddition();
-            if(result==true){
-                point=minMax(10,15);
-            } 
-        }
-        else if(actionType==2){
-            const result=askSubstraction();
-            if(result==true){
-                point=minMax(15,20)
-            }
-        }
-        else if(actionType==3){
-            const result=askMultiplication();
-            if(result==true){
-                point=minMax(20,25)
-            }
-        }
-        else if(actionType==4){
-            const result=askDivision();
-            if(result==true){
-                point=minMax(20,30)
-            }
-        }
-        if(point==0){
-            console.log("You fumbled, no healing for you!");
-        } else{
-            console.log("You are healing " + point + " points");
-        }
-        if (isP1Turn) {
-            p1Health = p1Health + point;
-        } else {
-            p2Health = p2Health + point;
-        }
-    } else if (actionNumber == 2) {
-        let typeAttack: number;
-        if (isP1Turn) {
-            typeAttack = promptAttack(player1Actions);
-        } else {
-            typeAttack = promptAttack(player2Actions);
-        }
-        if(typeAttack==1){
-            const userGoodAnswer = askAddition();
-            if(userGoodAnswer == true){
-                const point = minMax(5, 10);
+interface Game {
+    isP1Turn: boolean;
+    roundNumber: number;
+}
 
-                console.log("You attacked the opponent with " + point + " points");
-                if (isP1Turn) {
-                    p2Health = p2Health - point;
-                } else {
-                    p1Health = p1Health - point;
-                }
-            }
-            else {
-                console.log("You attacked and missed! The other player did not get hit.");
-            }
-        } else if (typeAttack==2){
-            // Substraction
-            const userGoodAnswer = askSubstraction();
-            if(userGoodAnswer == true){
-                const point = minMax(8, 15);
-                console.log("You attacked the opponent with " + point + " points");
-                if (isP1Turn) {
-                    p2Health = p2Health - point;
-                } else {
-                    p1Health = p1Health - point;
-                }
-                if (isP1Turn) {
-                    player1Actions[0]=false;
-                } else {
-                    player2Actions[0]=false;
-                }
-            }else{
-                console.log("You attacked and missed! The other player did not get hit.");
-            }
-        } else if (typeAttack==3){
-            const userGoodAnswer = askMultiplication();
-            if(userGoodAnswer == true){
-                const point = minMax(10, 20);
+const player1: Player = {
+    name: promptsync("Player 1 name: ") ?? "",
+    actions: [false, false, false, false],
+    health: minMax(48, 52)
+};
 
-                console.log("You attacked the opponent with " + point + " points");
-                if (isP1Turn) {
-                    p2Health = p2Health - point;
-                } else {
-                    p1Health = p1Health - point;
-                }
-                if (isP1Turn) {
-                    player1Actions[0]=false;
-                    player1Actions[1]=false;
-                } else {
-                    player2Actions[0]=false;
-                    player2Actions[1]=false;
-                }
-            }
-            else {
-                console.log("You attacked and missed! The other player did not get hit.");
-            }
-        }
-        else if (typeAttack==4){
-            const userGoodAnswer = askDivision();
-            if(userGoodAnswer == true){
-                const point = minMax(15, 25);
+const player2: Player = {
+    name: promptsync("Player 2 name: ") ?? "",
+    actions: [false, false, false, false],
+    health: minMax(48, 52)
+};
 
-                console.log("You attacked the opponent with " + point + " points");
-                if (isP1Turn) {
-                    p2Health = p2Health - point;
-                } else {
-                    p1Health = p1Health - point;
-                }
-                if (isP1Turn) {
-                    player1Actions[0]=false;
-                    player1Actions[1]=false;
-                    player1Actions[2]=false;
-                    player1Actions[3]=false;
-                } else {
-                    player2Actions[0]=false;
-                    player2Actions[1]=false;
-                    player2Actions[2]=false;
-                    player2Actions[3]=false;
-                }
-            }
-            else {
-                console.log("You attacked and missed! The other player did not get hit.");
-            }
-        }
-       
+const game: Game = {
+    isP1Turn: true,
+    roundNumber: 0
+};
+
+while (player1.health > 0 && player2.health > 0) {
+    game.roundNumber++;
+    showRoundTitle(game);
+    showPlayersHealth(game, player1, player2);
+    const actionNumber: Actions = askAction();
+
+    if (actionNumber == Actions.Heal) {
+        health(game, player1, player2);
+    } else if (actionNumber == Actions.Attack) {
+        attack(game, player1, player2);
     } else {
         console.log("You are loosing a turn by doing an unknown action!");
     }
 
     // Flip (reverse) the value. True goes False, False goes True. We alternate.
-    isP1Turn = !isP1Turn;
+    game.isP1Turn = !game.isP1Turn;
 }
 console.log("Game over");
+showWinner(player1, player2);
 
-if (p1Health > 0) {
-    console.log(player1Name + " won");
-} else {
-    console.log(player2Name + " won");
+function showWinner(player1: Player, player2: Player): void {
+    const winner: Player = player1.health>0 ? player1 : player2;
+    console.log(winner.name + " won");
 }
 
 // Random between min and max value
@@ -183,26 +74,42 @@ function minMax(minValue: number, maxValue: number): number {
 }
 
 // Ask the user what kind of attack he wants to do
-function promptAttack(listOfActions: boolean[]): number {
+function promptAttack(listOfActions: boolean[]): AttackTypes {
     let actionNumber: number = 0;
-    while (actionNumber === 0 || actionNumber > 4 || listOfActions[actionNumber-1]==true) {
+    while (actionNumber === 0 || actionNumber > 4 || listOfActions[actionNumber - 1] == true) {
         console.log("What type of attack do you want to do?");
-        if(listOfActions[0]==false){
+        if (listOfActions[0] == false) {
             console.log("1. Addition");
         }
-        if(listOfActions[1]==false){
+        if (listOfActions[1] == false) {
             console.log("2. Substraction");
         }
-        if(listOfActions[2]==false){
+        if (listOfActions[2] == false) {
             console.log("3. Multiplication");
         }
-        if(listOfActions[3]==false){
+        if (listOfActions[3] == false) {
             console.log("4. Division");
         }
         actionNumber = Number(promptsync("Choose an action: ") ?? "");
     }
-    listOfActions[actionNumber-1]=true;
-    return actionNumber;
+    let actionEnum: AttackTypes = AttackTypes.Unknown;
+    if (actionNumber == 1) {
+        actionEnum = AttackTypes.Addition;
+    } else if (actionNumber == 2) {
+        actionEnum = AttackTypes.Substraction;
+    }
+    else if (actionNumber == 3) {
+        actionEnum = AttackTypes.Multiplication;
+    }
+    else if (actionNumber == 4) {
+        actionEnum = AttackTypes.Division;
+    } else {
+        actionEnum = AttackTypes.Unknown;
+    }
+    if (actionEnum != AttackTypes.Unknown) {
+        listOfActions[actionNumber - 1] = true;
+    }
+    return actionEnum;
 }
 
 // Ask addition between 0-100.
@@ -214,8 +121,8 @@ function askAddition(): boolean {
         promptsync("What is " + leftSide + "+" + rightSide + "?") ?? "",
     );
     const isGoodValue = leftSide + rightSide == userResponse;
-    if(isGoodValue==false){
-        console.log("Unfortunately, the good answer was: "+ (leftSide +rightSide) );
+    if (isGoodValue == false) {
+        console.log("Unfortunately, the good answer was: " + (leftSide + rightSide));
     }
     return isGoodValue;
 }
@@ -227,10 +134,10 @@ function askSubstraction(): boolean {
         promptsync("What is " + leftSide + "-" + rightSide + "?") ?? "",
     );
     const isGoodValue = leftSide - rightSide == userResponse;
-    if(isGoodValue==false){
-         console.log("Unfortunately, the good answer was: "+ (leftSide -rightSide) );
-     } 
-     return isGoodValue;
+    if (isGoodValue == false) {
+        console.log("Unfortunately, the good answer was: " + (leftSide - rightSide));
+    }
+    return isGoodValue;
 }
 
 function askMultiplication(): boolean {
@@ -240,8 +147,8 @@ function askMultiplication(): boolean {
         promptsync("What is " + leftSide + "*" + rightSide + "?") ?? "",
     );
     const isGoodValue = leftSide * rightSide == userResponse;
-    if(isGoodValue==false){
-         console.log("Unfortunately, the good answer was: "+ (leftSide *rightSide) );
+    if (isGoodValue == false) {
+        console.log("Unfortunately, the good answer was: " + (leftSide * rightSide));
     }
     return isGoodValue;
 }
@@ -253,8 +160,170 @@ function askDivision(): boolean {
         promptsync("What is " + leftSide + "/" + rightSide + "?") ?? "",
     );
     const isGoodValue = result == userResponse;
-    if(isGoodValue==false){
-        console.log("Unfortunately, the good answer was: "+ (leftSide /rightSide) );
+    if (isGoodValue == false) {
+        console.log("Unfortunately, the good answer was: " + (leftSide / rightSide));
     }
     return isGoodValue;
+}
+
+function showRoundTitle(game: Game): void {
+    console.log("========================================");
+    console.log("================ Round " + game.roundNumber + " ==============");
+    console.log("========================================");
+}
+
+function showPlayersHealth(game: Game, player1: Player, player2: Player): void {
+    if (game.isP1Turn) {
+        console.log(
+            player1.name + " " + player1.health + " vs " + player2.health + " " + player2.name,
+        );
+    } else {
+        console.log(
+            player2.name + " " + player2.health + " vs " + player1.health + " " + player1.name,
+        );
+    }
+}
+
+function askAction(): Actions {
+    console.log("What do you want to do?");
+    console.log("1. Heal");
+    console.log("2. Attack");
+    const actionNumber: number = Number(promptsync("Choose an action: ") ?? "");
+    if (actionNumber == 1) {
+        return Actions.Heal
+    } else if (actionNumber == 2) {
+        return Actions.Attack;
+    } else {
+        return Actions.Unknown;
+    }
+}
+
+function health(game: Game, player1: Player, player2: Player):void {
+const actionType: AttackTypes = minMax(1, 4); // Random between 1 and 4
+    let point = 0;
+    if (actionType == AttackTypes.Addition) {
+        const result = askAddition();
+        if (result == true) {
+            point = minMax(10, 15);
+        }
+    }
+    else if (actionType == AttackTypes.Substraction) {
+        const result = askSubstraction();
+        if (result == true) {
+            point = minMax(15, 20)
+        }
+    }
+    else if (actionType == AttackTypes.Multiplication) {
+        const result = askMultiplication();
+        if (result == true) {
+            point = minMax(20, 25)
+        }
+    }
+    else if (actionType == AttackTypes.Division) {
+        const result = askDivision();
+        if (result == true) {
+            point = minMax(20, 30);
+        }
+    }
+    if (point == 0) {
+        console.log("You fumbled, no healing for you!");
+    } else {
+        console.log("You are healing " + point + " points");
+    }
+    if (game.isP1Turn) {
+        player1.health += point;
+    } else {
+        player2.health += point;
+    }
+}
+
+function attack(game: Game, player1: Player, player2: Player):void {
+    let typeAttack: AttackTypes;
+        if (game.isP1Turn) {
+            typeAttack = promptAttack(player1.actions);
+        } else {
+            typeAttack = promptAttack(player2.actions);
+        }
+        if (typeAttack == AttackTypes.Addition) {
+            const userGoodAnswer = askAddition();
+            if (userGoodAnswer == true) {
+                const point = minMax(5, 10);
+
+                console.log("You attacked the opponent with " + point + " points");
+                if (game.isP1Turn) {
+                    player2.health -= point;
+                } else {
+                    player2.health -= point;
+                }
+            }
+            else {
+                console.log("You attacked and missed! The other player did not get hit.");
+            }
+        } else if (typeAttack == AttackTypes.Substraction) {
+            // Substraction
+            const userGoodAnswer = askSubstraction();
+            if (userGoodAnswer == true) {
+                const point = minMax(8, 15);
+                console.log("You attacked the opponent with " + point + " points");
+                if (game.isP1Turn) {
+                    player2.health -= point;
+                } else {
+                    player1.health -= point;
+                }
+                if (game.isP1Turn) {
+                    player1.actions[0] = false;
+                } else {
+                    player2.actions[0] = false;
+                }
+            } else {
+                console.log("You attacked and missed! The other player did not get hit.");
+            }
+        } else if (typeAttack == AttackTypes.Multiplication) {
+            const userGoodAnswer = askMultiplication();
+            if (userGoodAnswer == true) {
+                const point = minMax(10, 20);
+
+                console.log("You attacked the opponent with " + point + " points");
+                if (game.isP1Turn) {
+                    player2.health -= point;
+                } else {
+                    player1.health -= point;
+                }
+                if (game.isP1Turn) {
+                    player1.actions[0] = false;
+                    player1.actions[1] = false;
+                } else {
+                    player2.actions[0] = false;
+                    player2.actions[1] = false;
+                }
+            }
+            else {
+                console.log("You attacked and missed! The other player did not get hit.");
+            }
+        }
+        else if (typeAttack == AttackTypes.Division) {
+            const userGoodAnswer = askDivision();
+            if (userGoodAnswer == true) {
+                const point = minMax(15, 25);
+
+                console.log("You attacked the opponent with " + point + " points");
+                if (game.isP1Turn) {
+                    player2.health -= point;
+                } else {
+                    player1.health -= point;
+                }
+         
+                resetAllActions(game.isP1Turn?player1:player2);
+            
+            }
+            else {
+                console.log("You attacked and missed! The other player did not get hit.");
+            }
+        }
+}
+function resetAllActions(player: Player):void {
+    player.actions[0] = false;
+    player.actions[1] = false;
+    player.actions[2] = false;
+    player.actions[3] = false;
 }
