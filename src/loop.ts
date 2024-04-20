382ed7264a2324714ebfad9dc62580dd825f4c8e
AliciaDesjardins <123902431+AliciaDesjardins@users.noreply.github.com>
2024-04-20 19:22:43 +0000

    loop

---
+++ src/loop.ts
@@ -0,0 +1,17 @@
+console.log("alicia");
+for(let i =1;i <= 10;i++){
+    console.log(i);
+}
+for(let i =0;i<=5;i++){
+    console.log(i)
+}
+for(let i =0;i<6;i++){
+    console.log("ali "+ (i+1));
+}
+
+for(let left =0;left<=1010;left++){
+    for(let right =0;right<5;right++){
+        console.log(left + " + " + right + " = " + (left+right))
+    }
+    console.log("GET OUT");
+}
