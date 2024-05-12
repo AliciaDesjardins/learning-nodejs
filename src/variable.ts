3575e94d4be720ddd7bad77046867e76c4ba5b4d
AliciaDesjardins <123902431+AliciaDesjardins@users.noreply.github.com>
2024-05-12 19:17:34 +0000

    Variables Exercices

---
+++ src/variable.ts
@@ -0,0 +1,16 @@
+const x = [1,2,3];
+const y = ["a","b"];
+const xx: number[] = [1,2,3];
+const a:string='alicia';
+const xxx: string[] =['5','9','36','40',a];
+console.log(xxx);
+xxx.push("Patrick");
+xxx.push("Jacob");
+// xxx[4]="Patrick";
+// xxx[8]="Jacob"
+console.log(xxx);
+xxx.pop();
+console.log(xxx);
+
+const xxxxx:number[] =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+console.log(xxxxx)
